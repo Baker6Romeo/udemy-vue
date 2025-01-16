5eb55dac9c1226e9665b6b3f9472951e88a181d1
Jonathan Shaw <jonathan.a.shaw8.civ@us.navy.mil>
2025-01-16 11:32:37 -0800

    Two-way binding on title

--- compositionApi/vue-composition-api-basics/src/views/HomeView.vue
+++ compositionApi/vue-composition-api-basics/src/views/HomeView.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
+
+    <h3>{{ counterTitle }}:</h3>
+
     <div>
     <button class="btn" @click="decreaseCounter">-</button>
       <span class="counter">{{ counter }}</span>
     <button class="btn" @click="increaseCounter">+</button>
     </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+<input
+      v-model="counterTitle"
+      type="text"
+    >    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-  const counter = ref(0);
+  const counter = ref(0),
+    counterTitle = ref('My Counter')
 
   const decreaseCounter = () => {
     counter.value--
@@ -30,5 +41,8 @@ import { ref } from 'vue';
   .btn, .counter {
     font-size: 40px;
     margin: 10px;
+  }
+  .edit {
+    margin-top: 60px
   }
 </style>
