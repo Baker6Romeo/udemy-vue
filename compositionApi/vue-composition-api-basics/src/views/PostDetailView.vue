fe4534f5d65ed6f78544d05e4911d69bd0b9e1ff
Jonathan Shaw <jonathan.a.shaw8.civ@us.navy.mil>
2025-01-23 08:11:20 -0800

    Access route and router procedurally

--- compositionApi/vue-composition-api-basics/src/views/PostDetailView.vue
+++ compositionApi/vue-composition-api-basics/src/views/PostDetailView.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="posts">
+  <div class="post-detail">
     <h1>Posts</h1>
-    <textarea v-autofocus />
+    <p>Display the content of post wit ID of {{ $route.params.id }}</p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
+    <div>
+      <button @click="goHome">Go home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to first post</button>
+    </div>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 
 <script setup>
+import { useRoute, useRouter } from 'vue-router';
 
-  import { vAutofocus } from '@/directives/vAutofocus';
+
+  const route = useRoute();
+  const router = useRouter();
+
+  const goHome = () => {
+    setTimeout(() => {router.push("/");}, 3000);    
+  }
+  const goToFirstPost = () => {
+    router.push({
+      name: 'postDetail',
+      params: {
+        id: '1'
+      }
+    })
+  }
+  const showPostId = () => {
+    alert(route.params.id);
+  }
 </script>
