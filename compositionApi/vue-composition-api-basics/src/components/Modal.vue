11c20c14c8e85a3d9f3c9d806afab188822524c6
Jonathan Shaw <jonathan.a.shaw8.civ@us.navy.mil>
2025-01-26 11:30:29 -0800

    Add child component

---
+++ compositionApi/vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <teleport to="body">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, eius ullam iusto est ipsam recusandae hic animi harum, nisi voluptates veritatis error perferendis. Culpa omnis distinctio corporis quod, molestiae ad?</p>
+      <button>Hide Modal</button>
+    </div>
+    </teleport>
+</template>
+
+<style>
+  .modal {
+    background: beige;
+    color: blue;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+</style>
--- compositionApi/vue-composition-api-basics/src/views/ModalsView.vue
+++ compositionApi/vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,32 +2,16 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show Modal</button>
-    <teleport to="body">
-    <div v-if="showModal" class="modal">
-      <h1>This is a modal</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, eius ullam iusto est ipsam recusandae hic animi harum, nisi voluptates veritatis error perferendis. Culpa omnis distinctio corporis quod, molestiae ad?</p>
-      <button @click="showModal = false">Hide Modal</button>
-    </div>
-    </teleport>
+    <Modal v-if="showModal"></Modal>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
+import Modal from '@/components/Modal.vue'
+
   const showModal = ref(false);
 
 </script>
 
-<style>
-  .modal {
-    background: beige;
-    color: blue;
-    padding: 10px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-</style>
